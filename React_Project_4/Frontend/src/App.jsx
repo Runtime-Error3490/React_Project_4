8c675c747a8998f3f377d9d7412009550d8d1a23
Runtime-Error3490 <shaleen.shukla@learner.manipal.edu>
2024-04-22 17:50:02 +0530

    first commit i missed 2 days damn .....

--- React_Project_4/Frontend/src/App.jsx
+++ React_Project_4/Frontend/src/App.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import styled from 'styled-components';
 export default function App() {
   return (
-    <div>App</div>
+    <Container>
+      <TopContainer>
+      <div className='logo'>
+        <img src="Photos/Foody Zone.svg" alt="Logo"/>
+      </div>
+      <div className='search'>
+        <input type='text' placeholder='Search food'/>
+      </div>
+      </TopContainer>
+    </Container>
   )
 }
+
+const Container=styled.div`
+max-width: 1200px;
+margin: 0 auto;
+`;
+const TopContainer=styled.div`
+min-height: 140px;
+display: flex;
+align-items: center;
+justify-content: space-around;
+padding:16px;
+.search{
+  input{
+    background-color: transparent;
+    border-radius: 5px;
+    height: 40px;
+    border: 1px solid red;
+    color: white;
+    font-size: 16px;
+    padding: 0 10px;
+  }
+}
+`;
--- React_Project_4/Frontend/src/main.jsx
+++ React_Project_4/Frontend/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {createGlobalStyle} from "styled-components";
 
+const GlobalStyle=createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    font-family: 'Poppins', sans-serif;
+  }
+  body{
+    background-color: #323334;
+    color: white;
+    min-height: 100vh;
+  }
+`;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <GlobalStyle/>
     <App />
   </React.StrictMode>,
 )
